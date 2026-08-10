84b298af94dc15f1f9b2e531844df5f492256c76
Muqthadir <mohammedmuqtadir41@gmail.com>
2026-08-10 20:02:23 +0530

    feat: add interview session model

---
+++ Backend/src/models/InterviewSession.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+
+const interviewSessionSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: true,
+    },
+    interviewReport: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "interviewReports",
+      required: true,
+    },
+    currentQuestion: {
+      type: String,
+      default: "",
+    },
+    questionsAsked: [
+      {
+        question: {
+          type: String,
+          required: true,
+        },
+        answer: {
+          type: String,
+          default: "",
+        },
+        feedback: {
+          type: String,
+          default: "",
+        },
+        score: {
+          type: Number,
+          min: 0,
+          max: 10,
+        },
+      },
+    ],
+
+    status: {
+      type: String,
+      enum: ["active", "completed"],
+      default: "active",
+    },
+    finalScore: {
+      type: Number,
+      min: 0,
+      max: 100,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const interviewSessionModel = mongoose.model(
+  "interviewSession",
+  interviewSessionSchema,
+);
